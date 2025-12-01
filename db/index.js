0594e21bf42676b348dcfdec875b0ea2da111f09
Sam Bassong <sam.bassong@gmail.com>
2025-12-01 11:47:59 -0800

    trying to fix rate limiting deployment error

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/authDatabase";
 
 mongoose
-  .connect(MONGODB_URI, {dbName: "cs-361"})
+  .connect(MONGODB_URI, {dbName: "cs-290"})
   .then(() => console.log("Successfully connected to MongoDB."))
   .catch((e) => console.error("Connection error", e.message));
 
